eb167d59dc334daec7cebaa2d9b224cfa518e2ea
frostvandrer <patrik.pis@gmail.com>
2023-06-12 11:28:30 +0200

    Config update

--- configure.sh
+++ configure.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev
-sudo apt-get install -y xfce-terminal
+sudo apt-get install -y xfce4-terminal
 
 ## Font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
@@ -39,14 +39,14 @@ cp .fehbg ~/.fehbg
 sudo apt install -y xsel xclip
 cp tmux/.tmux.conf ~/.tmux.conf
 
-# Terminal
-## ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Home dir cleanup
+rm -rf ~/Templates ~/Videos ~/Pictures ~/Music ~/Desktop
 
+# Terminal
 ## Nord Theme
 git clone https://github.com/nordtheme/xfce-terminal.git
 cd xfce-terminal
 ./install.sh
 
-# Home dir cleanup
-rm -rf ~/Templates ~/Videos ~/Pictures ~/Music ~/Desktop
+## ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
